475a821266dd834aa93860ae96f3e552a5216255
MateoBermudez <mateo.bermudez2910@gmail.com>
2024-04-24 17:45:32 -0500

    Primer commit
    Agregar Registro e Inicio de Usuario en la interfaz grafica sin clases logicas.

---
+++ src/main/java/Vista/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vista;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+    public static void main(String[] args) {
+        InicioUsuarioForm InicioUsuario = new InicioUsuarioForm();
+        InicioUsuario.InitLogin();
+    }
+}
