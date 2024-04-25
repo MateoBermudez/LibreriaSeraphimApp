159f9acc90a0ae4842bcdaaee0f55dc6dd3b3b6e
Emma12212006 <emmanuel_arango82231@elpoli.edu.co>
2024-04-25 09:28:29 -0500

    Prueba

--- src/main/java/Vista/Main.java
+++ src/main/java/Vista/Main.java
@@ -12,5 +12,6 @@ public class Main {
     public static void main(String[] args) {
         InicioUsuarioForm InicioUsuario = new InicioUsuarioForm();
         InicioUsuario.InitLogin();
+        System.out.println("Hola");
     }
 }
