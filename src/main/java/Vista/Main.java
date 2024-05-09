a599ba53dcdc9492fabd2baef4ade65fad0696ba
Emma12212006 <emmanuel_arango82231@elpoli.edu.co>
2024-05-09 09:46:59 -0500

    Creada la interfaz grafica de la tienda

--- src/main/java/Vista/Main.java
+++ src/main/java/Vista/Main.java
@@ -12,5 +12,7 @@ public class Main {
     public static void main(String[] args) {
         InicioUsuarioForm InicioUsuario = new InicioUsuarioForm();
         InicioUsuario.InitLogin();
+        libreriaForm libreria = new libreriaForm();
+        libreria.setVisible(true);
     }
 }
